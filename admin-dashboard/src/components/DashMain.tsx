9e7352206e78b2a0e5f7b95726acf9de5a70b9a6
Superior <samsonaderonmu2021@gmail.com>
2024-10-28 16:21:27 +0100

    Refactor DashMain component to remove unused import

--- admin-dashboard/src/components/DashMain.tsx
+++ admin-dashboard/src/components/DashMain.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
