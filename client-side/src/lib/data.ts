cfe4889352e524246a5aa6717216c262c0e4e39c
Superior <samsonaderonmu2021@gmail.com>
2024-11-08 09:49:21 +0100

    Update product images in data.ts for consistency and improved presentation

--- client-side/src/lib/data.ts
+++ client-side/src/lib/data.ts
@@ -2,25 +2,25 @@ export const products = [
   {
     id: 1,
     title: "INSPIRING FAITH PLANNER AND JOURNAL (NOVEMBER-DECEMBER 2024)",
-    image: "/1.png",
+    image: "/3.png",
     color: "bg-[#D6D8DB]",
   },
   {
     id: 2,
     title: "INSPIRING FAITH PLANNER AND JOURNAL (JANUARY-APRIL 2025)",
-    image: "/2.png",
+    image: "/4.png",
     color: "bg-[#BFF01A]",
   },
   {
     id: 3,
     title: "INSPIRING FAITH PLANNER AND JOURNAL (MAY-AUGUST 2025)",
-    image: "/3.png",
+    image: "/2.png",
     color: "bg-[#D6D8DB]",
   },
   {
     id: 4,
     title: "INSPIRING FAITH PLANNER AND JOURNAL (SEPTEMBER-DECEMBER 2025)",
-    image: "/4.png",
+    image: "/1.png",
     color: "bg-[#BFF01A]",
   },
 ];
