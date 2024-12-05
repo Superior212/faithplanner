02035cf4882a1cc49e3fc58387b28414ab42baad
Superior <samsonaderonmu2021@gmail.com>
2024-12-05 08:41:48 +0100

    Refactor PreOrderBanner component to improve layout and functionality

--- client-side/src/components/PreOrderBanner.tsx
+++ client-side/src/components/PreOrderBanner.tsx
@@ -1,26 +1,33 @@
-
-// import { usePreOrderBanner } from "@/hooks/usePreOrderBanner";
+import { usePreOrderBanner } from "@/hooks/usePreOrderBanner";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function PreOrderBanner() {
-//   const { isVisible, setIsVisible } = usePreOrderBanner();
+  const router = useRouter();
+  const { isVisible, setIsVisible } = usePreOrderBanner();
 
-//   if (!isVisible) return null;
+  if (!isVisible) return null;
 
   return (
     <div className="bg-[#CCFF00] text-black font-[700] py-6 px-4">
-      <div className="container mx-auto flex items-center justify-center">
-        <p className="text-sm font-medium">Pre-order your 2025 planner now!</p>
-        {/* <div className="flex items-center space-x-4">
-          <Button variant="secondary" size="sm" className="text-xs">
+      <div className="container mx-auto flex items-center justify-between">
+        <p className="sm:text-base text-sm font-medium">
+          Pre-order your 2025 planner now!
+        </p>
+        <div className="flex items-center space-x-4">
+          <Button
+           onClick={() => {
+            router.push("/products")}}  size="sm" className="text-xs">
             Order Now
           </Button>
           <button
             onClick={() => setIsVisible(false)}
             className="text-primary-foreground/80 hover:text-primary-foreground transition-colors"
             aria-label="Close announcement">
-            <X size={18} />
+            <X className="text-black" size={18} />
           </button>
-        </div> */}
+        </div>
       </div>
     </div>
   );
