e42fc0015a272dac388e4ad2836d33c4f732ebf2
Superior <samsonaderonmu2021@gmail.com>
2024-12-30 20:26:18 +0100

    Comment out PayPal SDK configuration and related service methods for future reference

--- server-side/Services/paypalService.js
+++ server-side/Services/paypalService.js
@@ -1,53 +1,53 @@
-const paypal = require('../config/paypal');
+// const paypal = require('../config/paypal');
 
-class PayPalService {
-    async createOrder(items, amount) {
-        const create_payment_json = {
-            "intent": "sale",
-            "payer": {
-                "payment_method": "paypal"
-            },
-            "transactions": [{
-                "amount": {
-                    "currency": "USD",
-                    "total": amount.toString()
-                },
-                "description": "Your order description"
-            }]
-        };
+// class PayPalService {
+//     async createOrder(items, amount) {
+//         const create_payment_json = {
+//             "intent": "sale",
+//             "payer": {
+//                 "payment_method": "paypal"
+//             },
+//             "transactions": [{
+//                 "amount": {
+//                     "currency": "USD",
+//                     "total": amount.toString()
+//                 },
+//                 "description": "Your order description"
+//             }]
+//         };
 
-        return new Promise((resolve, reject) => {
-            paypal.payment.create(create_payment_json, function (error, payment) {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(payment);
-                }
-            });
-        });
-    }
+//         return new Promise((resolve, reject) => {
+//             paypal.payment.create(create_payment_json, function (error, payment) {
+//                 if (error) {
+//                     reject(error);
+//                 } else {
+//                     resolve(payment);
+//                 }
+//             });
+//         });
+//     }
 
-    async capturePayment(paymentId, payerId) {
-        const execute_payment_json = {
-            "payer_id": payerId,
-            "transactions": [{
-                "amount": {
-                    "currency": "USD",
-                    "total": amount.toString()
-                }
-            }]
-        };
+//     async capturePayment(paymentId, payerId) {
+//         const execute_payment_json = {
+//             "payer_id": payerId,
+//             "transactions": [{
+//                 "amount": {
+//                     "currency": "USD",
+//                     "total": amount.toString()
+//                 }
+//             }]
+//         };
 
-        return new Promise((resolve, reject) => {
-            paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(payment);
-                }
-            });
-        });
-    }
-}
+//         return new Promise((resolve, reject) => {
+//             paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+//                 if (error) {
+//                     reject(error);
+//                 } else {
+//                     resolve(payment);
+//                 }
+//             });
+//         });
+//     }
+// }
 
-module.exports = new PayPalService();
+// module.exports = new PayPalService();
